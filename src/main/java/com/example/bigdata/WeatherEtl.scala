344762acd0df953cadcea61a5be04804270e7a3f
Paweł Korobczyński <pawel.korobczynski@gmail.com>
2021-01-19 16:43:22 +0100

    add distinct to weatherEtl

--- src/main/java/com/example/bigdata/WeatherEtl.scala
+++ src/main/java/com/example/bigdata/WeatherEtl.scala
@@ -18,6 +18,7 @@ object WeatherEtl {
       option("header", false).option("inferSchema", false).
       load(path + s"/weather.txt").
       select(trim(split($"value", ":").getItem(2)).as("weather_conditions")).
+      distinct().
       where(not($"weather_conditions" === "null" or $"weather_conditions" === "Unknown")).
       withColumn("weather_category", when($"weather_conditions".contains("Fine"), "favorable").otherwise("unfavorable")).
       withColumn("weather_id", functions.monotonically_increasing_id()).
